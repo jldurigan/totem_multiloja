887d0e09e724885c907b0e7bd550abad256ba818
João Lucas A. Durigan <lucasdurigan@hotmail.com>
2023-05-04 23:20:45 -0300

    adicionado primeiras rotas

--- 1 - totem_multiloja/settings.py
+++ 1 - totem_multiloja/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'totem_cliente',
+    'totem_restaurante',
 ]
 
 MIDDLEWARE = [
