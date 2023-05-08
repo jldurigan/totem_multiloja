ca656bec2f997c44263cbb34608bcd829f5c282e
João Lucas A. Durigan <lucasdurigan@hotmail.com>
2023-05-07 22:31:42 -0300

    ajustes admin e rotas

--- setup/urls.py
+++ setup/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('totem_cliente.urls')),
     path('cliente/', include('totem_cliente.urls')),
     path('restaurante/', include('totem_restaurante.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
