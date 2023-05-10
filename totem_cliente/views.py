2652643ccb1724824b25fb6989c1e11dc240aef6
stephuz <stephanienpaulino@gmail.com>
2023-05-10 00:59:08 -0300

    atualização 09-05

--- totem_cliente/views.py
+++ totem_cliente/views.py
@@ -6,3 +6,12 @@ def listar_restaurantes(request):
     restaurantes = Restaurante.objects.order_by("nome").filter(ativo=True)
 
     return render(request, "totem_cliente/inicio.html", {"restaurantes": restaurantes})
+
+def listar_produtos(request):
+    return render(request, "totem_cliente/produtos.html")
+
+def listar_carrinho(request):
+    return render(request, "totem_cliente/carrinho.html")
+
+def listar_pagamento(request):
+    return render(request, "totem_cliente/pagamento.html")
