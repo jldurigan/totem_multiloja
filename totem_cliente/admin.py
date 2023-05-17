55ce2d42193419c50f85e9954f8cb9a663a7c04e
João Lucas <jlucasdurigan@gmail.com>
2023-05-16 21:27:25 -0300

    reboot models

--- totem_cliente/admin.py
+++ totem_cliente/admin.py
@@ -9,45 +9,17 @@ class ListarRestaurantes(admin.ModelAdmin):
     list_per_page=10
     
 class ListarCategorias(admin.ModelAdmin):
-    list_display=("id", "nome", "imagem", "restaurante", "visivel", "ativo")
+    list_display=("id", "nome", "imagem", "restaurante", "ativo")
     list_display_links=("id", "nome")
-    list_editable=("imagem", "restaurante", "visivel", "ativo")
+    list_editable=("imagem", "restaurante", "ativo")
     search_fields=("nome",)
     list_per_page=10
     
-class ListarIngredientes(admin.ModelAdmin):
-    list_display=("id", "nome", "preco_extra", "imagem", "ativo")
-    list_display_links=("id", "nome")
-    list_editable=("preco_extra", "imagem", "ativo")
-    search_fields=("nome", "categoria__nome")
-    list_per_page=10
-    
 class ListarProdutos(admin.ModelAdmin):
-    list_display=("id", "nome", "descricao", "preco", "imagem", "restaurante", "visivel", "ativo")
-    list_display_links=("id", "nome")
-    list_editable=("preco", "restaurante", "visivel", "ativo")
-    search_fields=("nome", "categoria__nome", "restaurante__nome")
-    list_per_page=10
-    
-class ListarProdutoIngredientes(admin.ModelAdmin):
-    list_display=("id", "produto", "ingrediente", "quantidade_padrao", "quantidade_minima", "quantidade_maxima", "ativo")
-    list_display_links=("id", "produto")
-    list_editable=("ingrediente", "quantidade_padrao", "quantidade_minima", "quantidade_maxima", "ativo")
-    search_fields=("nome", "produto__nome", "ingrediente__nome")
-    list_per_page=10
-    
-class ListarCombos(admin.ModelAdmin):
     list_display=("id", "nome", "descricao", "preco", "imagem", "restaurante", "ativo")
     list_display_links=("id", "nome")
-    list_editable=("descricao", "preco", "imagem", "restaurante", "ativo")
-    search_fields=("nome", "produto__nome", "categoria__nome", "restaurante__nome")
-    list_per_page=10
-    
-class ListarComboProdutos(admin.ModelAdmin):
-    list_display=("id", "combo", "produto", "quantidade", "alternativa", "principal", "ativo")
-    list_display_links=("id", "combo")
-    list_editable=("produto", "quantidade", "alternativa", "principal", "ativo")
-    search_fields=("nome", "combo__nome")
+    list_editable=("preco", "restaurante", "ativo")
+    search_fields=("nome", "categoria__nome", "restaurante__nome")
     list_per_page=10
     
 class ListarPedidos(admin.ModelAdmin):
@@ -57,24 +29,13 @@ class ListarPedidos(admin.ModelAdmin):
     list_per_page=10
     
 class ListarItemPedidos(admin.ModelAdmin):
-    list_display=("id", "pedido", "produto", "combo", "quantidade", "preco", "ativo")
+    list_display=("id", "pedido", "produto", "quantidade", "preco", "ativo")
     list_display_links=("id", "pedido")
-    search_fields=("pedido__codigo_interno", "produto__nome", "combo__nome")
-    list_per_page=10
-    
-class ListarItemAlteracoes(admin.ModelAdmin):
-    list_display=("id", "item", "ingrediente", "quantidade_alterada", "ativo")
-    list_display_links=("id", "item")
-    search_fields=("pedido__codigo_interno", "ingrediente__nome")
+    search_fields=("pedido__codigo_interno", "produto__nome")
     list_per_page=10
 
 admin.site.register(Restaurante, ListarRestaurantes)
 admin.site.register(Categoria, ListarCategorias)
-admin.site.register(Ingrediente, ListarIngredientes)
 admin.site.register(Produto, ListarProdutos)
-admin.site.register(ProdutoIngrediente, ListarProdutoIngredientes)
-admin.site.register(Combo, ListarCombos)
-admin.site.register(ComboProduto, ListarComboProdutos)
 admin.site.register(Pedido, ListarPedidos)
 admin.site.register(ItemPedido, ListarItemPedidos)
-admin.site.register(ItemAlteracao, ListarItemAlteracoes)
--- totem_cliente/models.py
+++ totem_cliente/models.py
@@ -12,20 +12,7 @@ class Restaurante(models.Model):
 class Categoria(models.Model):
     nome = models.CharField(max_length=40, null=False, blank=False)
     imagem = models.ImageField(upload_to='imagens/categorias/%Y/%m/%d/', blank=True)
-    visivel = models.BooleanField(default=True)
     restaurante = models.ForeignKey(Restaurante, on_delete=models.CASCADE)
-    ativo = models.BooleanField(default=True)
-    
-    def __str__(self):
-        return self.nome
-
-
-class Ingrediente(models.Model):
-    nome = models.CharField(max_length=40, null=False, blank=False)
-    preco_extra = models.DecimalField(max_digits=4, decimal_places=2)
-    imagem = models.ImageField(upload_to='imagens/ingredientes/%Y/%m/%d/', blank=True)
-    restaurante = models.ForeignKey(Restaurante, on_delete=models.CASCADE)
-    categoria = models.ManyToManyField(Categoria)
     ativo = models.BooleanField(default=True)
     
     def __str__(self):
@@ -38,47 +25,11 @@ class Produto(models.Model):
     preco = models.DecimalField(max_digits=7, decimal_places=2)
     imagem = models.ImageField(upload_to='imagens/produtos/%Y/%m/%d/', blank=True)
     restaurante = models.ForeignKey(Restaurante, on_delete=models.CASCADE)
-    categoria = models.ManyToManyField(Categoria)
-    visivel = models.BooleanField(default=True)
+    categoria = models.ManyToManyField(Categoria)    
     ativo = models.BooleanField(default=True)
     
     def __str__(self):
         return self.nome
-    
-class ProdutoIngrediente(models.Model):
-    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
-    ingrediente = models.ForeignKey(Ingrediente, on_delete=models.CASCADE)
-    quantidade_padrao = models.PositiveIntegerField(default=0)
-    quantidade_minima = models.PositiveIntegerField(default=0)
-    quantidade_maxima = models.PositiveIntegerField(default=1)
-    ativo = models.BooleanField(default=True)
-
-    def __str__(self):
-        return f"{self.produto.nome} - {self.ingrediente.nome}"
-
-
-class Combo(models.Model):
-    nome = models.CharField(max_length=100, null=False, blank=False)
-    descricao = models.TextField(max_length=500, blank=False, null=False)
-    preco = models.DecimalField(max_digits=7, decimal_places=2)
-    imagem = models.ImageField(upload_to='imagens/combos/%Y/%m/%d/', blank=True)
-    restaurante = models.ForeignKey(Restaurante, on_delete=models.CASCADE)
-    categoria = models.ManyToManyField(Categoria)
-    ativo = models.BooleanField(default=True)
-    
-    def __str__(self):
-        return self.nome
-
-class ComboProduto(models.Model):
-    combo = models.ForeignKey(Combo, on_delete=models.CASCADE)
-    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
-    quantidade = models.PositiveIntegerField(default=0)
-    alternativa = models.BooleanField(default=False)
-    principal = models.BooleanField(default=False)
-    ativo = models.BooleanField(default=True)
-
-    def __str__(self):
-        return f"{self.combo.nome} - {self.produto.nome}"
 
 
 class Pedido(models.Model):
@@ -97,20 +48,10 @@ class Pedido(models.Model):
 class ItemPedido(models.Model):
     pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
     produto = models.ForeignKey(Produto, null=True, blank=True, on_delete=models.PROTECT)
-    combo = models.ForeignKey(Combo, null=True, blank=True, on_delete=models.PROTECT)
     quantidade = models.PositiveIntegerField(default=1)
     preco = models.DecimalField(max_digits=7, decimal_places=2)
     ativo = models.BooleanField(default=True)
     
     def __str__(self):
         return f"{self.pedido.restaurante.nome} Pedido {self.pedido.codigo_interno} Item {self.id}"
-    
-class ItemAlteracao(models.Model):
-    item = models.ForeignKey(ItemPedido, on_delete=models.CASCADE)
-    ingrediente = models.ForeignKey(Ingrediente, on_delete=models.PROTECT)
-    quantidade_alterada = models.PositiveIntegerField(default=0)
-    ativo = models.BooleanField(default=True)
-    
-    def __str__(self):
-        return f"Pedido: {self.item.pedido.codigo_interno} {self.ingrediente.nome} - Quantidade: {self.quantidade_alterada}"
     
